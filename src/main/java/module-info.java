0267c06b72878890b893ea85cd9ed87ed92bb45b
David <dsmonje@correo.url.edu.gt>
2024-10-28 11:43:19 -0600

    Created the proyect

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.java.fase2final_manejo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.java.fase2final_manejo to javafx.fxml;
+    exports org.java.fase2final_manejo;
+}
